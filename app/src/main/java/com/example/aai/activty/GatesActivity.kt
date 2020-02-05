768c4c04e9e87eaa1079f5d7b02a36f1bafcd493
apoorv-j <jainapoorv341@gmail.com>
2020-02-05 21:18:47 +0530

    Added sample pie chart

--- app/src/main/java/com/example/aai/activty/GatesActivity.kt
+++ app/src/main/java/com/example/aai/activty/GatesActivity.kt
@@ -2,12 +2,29 @@ package com.example.aai.activty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.example.aai.R
+import java.util.ArrayList
 
 class GatesActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gates)
+
+        val pie = AnyChart.pie()
+
+        val data = ArrayList<DataEntry>()
+        data.add(ValueDataEntry("John", 10000))
+        data.add(ValueDataEntry("Jake", 12000))
+        data.add(ValueDataEntry("Peter", 18000))
+
+        pie.data(data)
+
+        val anyChartView = findViewById<AnyChartView>(R.id.any_chart_view)
+        anyChartView.setChart(pie)
     }
 }
